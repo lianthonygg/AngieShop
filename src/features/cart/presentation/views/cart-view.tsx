9796deb24fbab0e2fa840306094d1e9e1d61c43a
Li Anthony <li.3108gomezgonzalez@gmail.com>
2026-01-26 22:22:32 -0500

    refactor: reorganize CartView layout for improved readability and structure

--- src/features/cart/presentation/views/cart-view.tsx
+++ src/features/cart/presentation/views/cart-view.tsx
@@ -30,27 +30,32 @@ const CartView = ({ data, totalItems, cartId }: CartViewProps) => {
   return (
     <div className="w-full min-h-screen flex flex-col">
       <Header cartCount={totalItems ?? 0} />
-      <section className="md:max-w-6xl mx-auto px-4 pt-6 pb-40 overflow-y-auto">
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-6 max-w-5xl mx-auto w-full px-4">
-          {data.items.map((item) => (
-            <CartItem
-              key={item.id}
-              id={item.product.id}
-              title={item.product.name}
-              price={item.product.price}
-              quantity={item.quantity}
-              imageUrl={item.product.image_url}
-              onRemove={handleRemoveCartItem}
-              onQuantityChange={handleUpdateQuantity}
-            />
-          ))}
+
+      <section className="flex-1 overflow-y-auto">
+        <div className="max-w-6xl mx-auto px-4 pt-6 pb-32">
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 max-w-5xl mx-auto w-full">
+            {data.items.map((item) => (
+              <CartItem
+                key={item.id}
+                id={item.product.id}
+                title={item.product.name}
+                price={item.product.price}
+                quantity={item.quantity}
+                imageUrl={item.product.image_url}
+                onRemove={handleRemoveCartItem}
+                onQuantityChange={handleUpdateQuantity}
+              />
+            ))}
+          </div>
+
+          <CheckoutButton
+            items={data.items}
+            onFinish={handleCompletePurchase}
+            cartId={cartId}
+          />
         </div>
-        <CheckoutButton
-          items={data.items}
-          onFinish={handleCompletePurchase}
-          cartId={cartId}
-        />
       </section>
+
       <BottomBar />
     </div>
   );
